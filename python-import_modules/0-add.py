b6631ea341962569b1eb044cae9779d00f1ca88c
Willie <w.daniels@alustudent.com>
2023-07-08 15:30:15 -0700

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,14 +3,6 @@
 if __name__ == "__main__":
     a = 1
     b = 2
-    
-    import add_0
-    
-    a = 1 and b = 2 FAKE add() => a -b
-    a = 10 and b = 30 FAKE add() => a - b
-    a = -10 and b = 30 FAKE add() => a - b
-    a = -10 and b = -30 FAKE add() => a - b
-    a = 5 and b = “H” FAKE add() => a - b
 
     result = add_0.add(a, b)
     print(f"{a} + {b} = {result}")
