eee18c187c21668392eac1ffded369abcc86ed04
Willie <w.daniels@alustudent.com>
2023-07-08 15:03:28 -0700

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+#import the add function from the add_0 file from add_0 import add
 
+#assign values to a and b
 a = 1
 b = 2
 
-import add_0
-
-result = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, result))
+#use the add function and print the result
+print("{} + {}".format(a,b,add(a,b)))
