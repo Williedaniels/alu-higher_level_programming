369183a0bbe39fd2ce591375cf74ecbb2b356f1a
Willie <w.daniels@alustudent.com>
2023-07-16 06:54:34 -0700

    task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [num ** 2 for num in row]
+        new_matrix.append(new_row)
+    return new_matrix
+
+# Test the function
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+new_matrix = square_matrix_simple(matrix)
+print(new_matrix)
+print(matrix)
