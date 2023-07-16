07287f226e7ef7bc07515b5ed03f9dd6e25d7d10
Willie <w.daniels@alustudent.com>
2023-07-16 00:59:36 -0700

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    string = ''
-    for i in my_string:
-        if (i.lower()) == 'c':
-            continue
-        string += i
-        return string
+    result = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            result += char
+    return result
+
