0d307039719f46e2a0d08024a80fabfd290671ff
Williedaniels <w.daniels@alustudent.com>
2023-08-04 11:34:29 +0000

    task 1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
--- Creates user user_0d_1 with password user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+#!/bin/bash
+# sends a GET request to the URL, and displays the body of the response
+curl -sfL "$1"
