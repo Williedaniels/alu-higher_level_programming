8b36d2768d27f249853dbdd2dd3fb7eb4cfb4c52
Williedaniels <w.daniels@alustudent>
2023-08-18 16:59:03 +0200

    task 0

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,17 @@
+alu-higher_level_programming/
+│
+├── python-almost_a_circle/
+│   ├── models/
+│   │   ├── base.py
+│   │   └── ...
+│   │
+│   ├── tests/
+│   │   ├── test_models/
+│   │   │   ├── test_base.py
+│   │   │   └── ...
+│   │   │
+│   │   └── ...
+│   │
+│   └── README.md
+│
+└── ...
