badb518aa170e35d8503f5ba2b2c46a574f45dc7
Williedaniels <w.daniels@alustudent>
2023-10-08 01:20:29 +0200

    task 1-6

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,21 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-// Check if the correct number of arguments is provided
-if (process.argv.length !== 4) {
-  console.error('Usage: ./writeFile.js <file_path> "<text_to_write>"');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const textToWrite = process.argv[3];
-
-// Write the string to the file
-fs.writeFile(filePath, textToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error('Error:', err);
-  } else {
-    console.log(`"${textToWrite}" was successfully written to ${filePath}`);
-  }
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  if (err) throw err;
 });
